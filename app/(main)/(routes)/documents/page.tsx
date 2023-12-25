f7b8ddabd4c55659ada0dd6444c32123e728b045
Utkarsh Bhardwaj <ub2092000@gmail.com>
2023-12-25 23:54:27 +0530

    add schema and api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -5,10 +5,22 @@ import React from "react";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
   const { user } = useUser();
-  console.log(user);
+  const create = useMutation(api.documents.create);
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -25,9 +37,15 @@ const DocumentsPage = () => {
         alt="empty"
         className="hidden dark:block"
       />
-      <h2 className="text-lg font-medium">Welcome to {user?.firstName ? user?.firstName : user?.emailAddresses[0].emailAddress}&apos;s Potion</h2>
-      <Button>
-        <PlusCircle className="h-4 w-4 mr-2"/>
+      <h2 className="text-lg font-medium">
+        Welcome to{" "}
+        {user?.firstName
+          ? user?.firstName
+          : user?.emailAddresses[0].emailAddress}
+        &apos;s Potion
+      </h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
     </div>
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,10 +6,14 @@ import { usePathname } from "next/navigation";
 import React, { useRef, ElementRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const Navigation = () => {
   const isMobile = useMediaQuery("(max-width: 786px)");
   const pathname = usePathname();
+  const documents = useQuery(api.documents.get);
+
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
@@ -114,7 +118,11 @@ const Navigation = () => {
           <UserItem/>
         </div>
         <div className="mt-4">
-          <p>Documents</p>
+          {
+            documents?.map((document)=>(
+              <p key={document._id}>{document.title}</p>
+            ))
+          }
         </div>
         <div
           onMouseDown={handleMouseDown}
