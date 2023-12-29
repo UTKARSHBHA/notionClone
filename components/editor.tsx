d1b60edaaaa0ad552644bbc65f2b9a26ed2acb35
Utkarsh Bhardwaj <ub2092000@gmail.com>
2023-12-29 22:03:45 +0530

    add editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { useTheme } from "next-themes";
-// import {
-//   BlockNoteEditor,
-//   PartialBlock
-// } from "@blocknote/core";
-// import {
-//   BlockNoteView,
-//   useBlockNote
-// } from "@blocknote/react";
-// import "@blocknote/core/style.css";
+import {
+  BlockNoteEditor,
+  PartialBlock
+} from "@blocknote/core";
+import {
+  BlockNoteView,
+  useBlockNote
+} from "@blocknote/react";
+import "@blocknote/core/style.css";
 
 import { useEdgeStore } from "@/lib/edgestore";
 
@@ -35,24 +35,24 @@ const Editor = ({
     return response.url;
   }
 
-//   const editor: BlockNoteEditor = useBlockNote({
-//     editable,
-//     initialContent: 
-//       initialContent 
-//       ? JSON.parse(initialContent) as PartialBlock[] 
-//       : undefined,
-//     onEditorContentChange: (editor) => {
-//       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
-//     },
-//     uploadFile: handleUpload
-//   })
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: 
+      initialContent 
+      ? JSON.parse(initialContent) as PartialBlock[] 
+      : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+    uploadFile: handleUpload
+  })
 
   return (
     <div>
-      {/* <BlockNoteView
+      <BlockNoteView
         editor={editor}
         theme={resolvedTheme === "dark" ? "dark" : "light"}
-      /> */}
+      />
     </div>
   )
 }
